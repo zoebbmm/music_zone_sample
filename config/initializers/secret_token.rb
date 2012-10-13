83db4641ffdfc84b82bcd563de203c4ccffdee54
zhou <zhou.wei@sv.cmu.edu>
2012-10-13 19:05:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicZoneSample::Application.config.secret_token = '86d346a34df1ce4a92bbd37c773e2e01b9546e3284ae8add0c8785a4ee86ff0a41a90d3a6d4eea3e07a67f82f0ef2e549f9368649631d665422d1cb77bdae1c9'
